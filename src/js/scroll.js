fee538036b4b950859c505f11a3d53182e9d5bb6
AlainKRSS <cvt.alain@gmail.com>
2017-03-29 10:39:35 +0200

    Remove unused JS & CSS

--- src/js/scroll.js
+++
@@ -1,6 +0,0 @@
-var section0 = document.querySelector('#section0');
-var section1 = document.querySelector('#section1');
-var section2 = document.querySelector('#section2');
-var logo = document.querySelector('a.logo');
-var side = document.querySelectorAll('ul.side li a');
-
