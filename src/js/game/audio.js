1be6962fbf6fe119334034315acb82b942f1e1f0
Pikog <perso.pikog@gmail.com>
2017-03-30 21:46:48 +0200

    Update sounds

--- src/js/game/audio.js
+++ src/js/game/audio.js
@@ -2,6 +2,8 @@ function Audio() {
   this.explosion = $("audio#explosion")[0];
   this.fail = $("audio#fail")[0];
   this.win = $("audio#win")[0];
+  this.tir = $("audio#tir")[0];
+  this.bonus = $("audio#bonus")[0];
   
 }
 
@@ -11,13 +13,21 @@ Audio.prototype.explosionSound = function () {
 };
 
 Audio.prototype.winSound = function () {
-  this.theme.pause();
   this.win.currentTime = 0;
   this.win.play();
 };
 
 Audio.prototype.failSound = function () {
-  this.theme.pause();
   this.fail.currentTime = 0;
   this.fail.play();
+};
+
+Audio.prototype.shootSound = function () {
+  this.tir.currentTime = 0;
+  this.tir.play();
+};
+
+Audio.prototype.bonusSound = function () {
+  this.bonus.currentTime = 0;
+  this.bonus.play();
 };
